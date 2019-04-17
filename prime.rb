54f8fa657e42ae5debaa4e64d30cfbc148a762f7
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 18:48:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 
 def prime?(number)
-  prime = true
-    for r in 2..Math.sqrt(number).to_i
-      if (number % r == 0)
-        prime = false
-        break
-      end
+  count = 2
+    while count < number
+          if number % count
+            false
+          else
+            true
+          end
     end
-    return prime
   end
