704ee6f2427752ec0f7cc5a3f86908e2477053e6
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 18:30:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,12 @@
 # Add  code here!
-def prime?(number)
 
-end
-
+def prime?()
+  for r in 2..Math.sqrt(self).to_i
+      if (self % r == 0)
+        prime = false
+        break
+      end
+    end
+    return prime
+  end
 end
